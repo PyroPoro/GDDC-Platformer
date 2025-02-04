755744b69bb34facd9c2f2b6a933147f19c3b794
Ricky Wen <48633781+PyroPoro@users.noreply.github.com>
2025-02-04 13:01:13 -0500

    workshop 3

--- Assets/Scripts/Game/ParallaxController.cs
+++ Assets/Scripts/Game/ParallaxController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxController : MonoBehaviour {
-    //[SerializeField] List<GameObject> backgrounds = new();
     [SerializeField] Transform cam;
     [SerializeField] float scrollingSpeed;
     [SerializeField] float backgroundWidth;
